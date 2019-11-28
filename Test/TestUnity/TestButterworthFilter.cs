d0a5f36704fd560215789deb3b1d580ba7eaf3fa
Christian <christian@realresponse.com.au>
2019-11-28 12:54:50 +1100

    Add Unit test for Butterworth Filter.

---
+++ Test/TestUnity/TestButterworthFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Pyro.TestUnity
+{
+    [TestFixture]
+    public class AppTests
+    {
+        [SetUp]
+        public void BeforeEachTest()
+        {
+        }
+
+        // see graph at https://docs.google.com/spreadsheets/d/1rVPEuRXezlsEIURaEMnihVuJJ8yexntQ_QcFNBdj1dc/edit#gid=0
+        [Test]
+        public void FilteredFloat()
+        {
+            var x = new FilterButterworth(1, 20 , FilterButterworth.PassType.Lowpass);
+            for (var i = 0; i < 40; ++i)
+            {
+                x.Update(1);
+                Console.WriteLine($"{x.Value}");
+            }
+            for (var i = 0; i < 40; ++i)
+            {
+                x.Update(0);
+                Console.WriteLine($"{x.Value}");
+            }
+        }
+    }
+}
